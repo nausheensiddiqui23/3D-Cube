3c8629867dcaf9f97b002cc86a73e76921e407e8
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-06 22:16:09 +0530

    new commit

--- script.js
+++ script.js
@@ -24,3 +24,8 @@ function animate() {
 }
 
 animate();
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
